a5458e61f421825ffd2e381bf28d333d51d8f2f8
Elijahscriptdev <elijahobominuru@gmail.com>
2025-02-26 10:41:30 +0100

    added jest for testing

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+};
