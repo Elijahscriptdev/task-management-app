c51d397ef9ed327ca27afa707acbce242925ec4b
Elijahscriptdev <elijahobominuru@gmail.com>
2025-02-26 10:02:47 +0100

    fixed a bug on filterering todo

--- src/components/TaskFilter.tsx
+++ src/components/TaskFilter.tsx
@@ -20,7 +20,7 @@ const TaskFilter: React.FC<TaskFilterProps> = React.memo(({ filter, setFilter })
           className="block w-full px-4 py-3 rounded-lg bg-gray-50 border border-gray-200 form-input-shadow focus:border-primary focus:ring-2 focus:ring-primary/20 outline-none"
         >
           <option value="all">All</option>
-          <option value="to-do">To Do</option>
+          <option value="todo">To Do</option>
           <option value="in-progress">In Progress</option>
           <option value="done">Done</option>
         </select>
